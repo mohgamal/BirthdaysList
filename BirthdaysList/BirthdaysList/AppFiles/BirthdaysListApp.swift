2a63a71100823dde4e071aeb5e48f43151b87ec3
Mohammed Abdelaty <mohammed.a@azenia.com>
2022-02-17 18:41:43 +0200

    create project and organize project groups

---
+++ BirthdaysList/BirthdaysList/AppFiles/BirthdaysListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysListApp.swift
+//  BirthdaysList
+//
+//  Created by Mohammed Abd El-Aty on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
