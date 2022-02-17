834c45664cc3b8490cca6bd0d41126546190a1ae
Mohammed Abdelaty <mohammed.a@azenia.com>
2022-02-17 19:05:46 +0200

    adding getBirthdaysList functionality

--- BirthdaysList/BirthdaysList/Services/BirthdaysListService.swift
+++ BirthdaysList/BirthdaysList/Services/BirthdaysListService.swift
@@ -13,7 +13,7 @@ protocol BirthdaysListServiceProtocol {
     func getBirthdaysList(handler: @escaping (Result<BirthdaysListModel, BirthdaysAppErrors>) -> Void)
 }
 
-class BirhtdaysListService: BirthdaysListServiceProtocol {
+class BirthdaysListService: BirthdaysListServiceProtocol {
 
     let urlString: String
 
@@ -22,6 +22,44 @@ class BirhtdaysListService: BirthdaysListServiceProtocol {
     }
 
     func getBirthdaysList(handler: @escaping (Result<BirthdaysListModel, BirthdaysAppErrors>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            handler(.failure(BirthdaysAppErrors.NotFound))
+            return
+        }
 
+        let task  = URLSession.shared.dataTask(with: url) { (data, response, error) in
+
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200...299:
+                    break
+                case 400:
+                    handler(.failure(BirthdaysAppErrors.BadRequest))
+                case 404:
+                    handler(.failure(BirthdaysAppErrors.NotFound))
+                case 500:
+                    handler(.failure(BirthdaysAppErrors.InternalServerError))
+                default:
+                    handler(.failure(BirthdaysAppErrors.InternalServerError))
+                }
+            }
+
+            guard let data = data else {
+                if let error = error {
+                    handler(.failure(BirthdaysAppErrors.UnknownError(message: error.localizedDescription)))
+                }
+                return
+            }
+
+            let decoder = JSONDecoder()
+
+            do {
+                let result = try decoder.decode(BirthdaysListModel.self, from: data)
+                handler(.success(result))
+            } catch {
+                handler(.failure(BirthdaysAppErrors.WrongData))
+            }
+        }
+        task.resume()
     }
 }
